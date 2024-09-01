ef0ada773e02026685dbb6d2d4ad8e3bd9908f94
siddharth1201 <siddharthverma1201@gmail.com>
2024-09-01 19:26:00 +0530

    vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // Replace with your backend URL
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        // Proxy API calls to backend server in development
+        '/api': {
+          target: mode === 'development'
+            ? 'https://blog-serve-production.up.railway.app/'
+            : 'https://your-production-backend-url.com/', // Modify the production URL if needed
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+    build: {
+      // Define build options if needed
+    },
+    define: {
+      // Environment variables can be set here
+      'http://localhost:5173/': JSON.stringify(mode === 'development' 
+        ? 'https://blog-serve-production.up.railway.app/' 
+        : 'https://your-production-backend-url.com/'),
+    },
+  };
+});
